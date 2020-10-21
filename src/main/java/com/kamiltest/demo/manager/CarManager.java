490e5a6afa53d59ca02c3924bc17d385545cebe0
Iceberg <hawxhawx1920@gmail.com>
2020-10-21 18:51:49 +0200

    working with services to make them crud for everything

--- src/main/java/com/kamiltest/demo/manager/CarManager.java
+++ src/main/java/com/kamiltest/demo/manager/CarManager.java
@@ -4,6 +4,8 @@ import com.kamiltest.demo.doa.Repo.CarRepo;
 import com.kamiltest.demo.doa.model.Car;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CarManager {
     private CarRepo carRepo;
@@ -12,7 +14,25 @@ public class CarManager {
         this.carRepo = carRepo;
     }
 
+    //get
     public Iterable<Car> findAll(){
         return this.carRepo.findAll();
+    }
+    public Optional<Car> findCarById(Long id) {return this.carRepo.findById(id);}
+    //add
+    //edit
+    public Car updateCar(Car car){
+        return this.carRepo.save(car);
+    }
+    //delete
+    public boolean deleteCarById(Long id){
+        try{
+            this.carRepo.deleteById(id);
+            return true;
+        }
+        catch(Exception ex){
+            System.out.println(ex.toString());
+            return false;
+        }
     }
 }
--- src/main/java/com/kamiltest/demo/manager/ClientManager.java
+++ src/main/java/com/kamiltest/demo/manager/ClientManager.java
@@ -1,6 +1,7 @@
 package com.kamiltest.demo.manager;
 
 import com.kamiltest.demo.doa.Repo.ClientRepo;
+import com.kamiltest.demo.doa.model.Car;
 import com.kamiltest.demo.doa.model.Client;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,11 @@ import java.util.Optional;
 @Service
 public class ClientManager {
     private ClientRepo clientRepo;
+    private CarManager carManager;
 
-    public ClientManager(ClientRepo clientRepo) {
+    public ClientManager(ClientRepo clientRepo, CarManager carManager) {
         this.clientRepo = clientRepo;
+        this.carManager = carManager;
     }
 
     public Optional<Client> findById(Long id){
@@ -28,5 +31,44 @@ public class ClientManager {
 
     public void delete(Long id){
         this.clientRepo.deleteById(id);
+    }
+
+    //delete Car for client
+    public boolean deleteCarForClient(Long idClient){
+        try{
+            Optional<Client> client = this.findById(idClient);
+            if(client.isPresent()){
+                Optional<Car> carToDelete = this.carManager.findCarById(client.get().getCar().getId());
+                if(carToDelete.isPresent()){
+                    this.carManager.deleteCarById(carToDelete.get().getId());
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        catch(Exception ex){
+            System.out.println(ex.toString());
+            return false;
+        }
+    }
+    //update car for client
+    //add car for client
+    public Client updateOrAddCarForClient(Long idClient,Car carToUpdateOrAdd){
+        try{
+            Optional<Client> client = this.findById(idClient);
+            if(client.isPresent() && carToUpdateOrAdd != null){
+                Client clientToUpdate = client.get();
+                clientToUpdate.setCar(carToUpdateOrAdd);
+                this.clientRepo.save(clientToUpdate);
+                return clientToUpdate;
+            }
+            return null;
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.toString());
+            return null;
+        }
     }
 }
