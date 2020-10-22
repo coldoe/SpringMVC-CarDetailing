d7bb4264e1693f89aa9e6e29f38cb27337dc614b
Iceberg <hawxhawx1920@gmail.com>
2020-10-22 17:59:34 +0200

    working with carcontroller

--- src/main/java/com/kamiltest/demo/doa/controller/CarController.java
+++ src/main/java/com/kamiltest/demo/doa/controller/CarController.java
@@ -2,24 +2,26 @@ package com.kamiltest.demo.doa.controller;
 
 import com.kamiltest.demo.doa.model.Car;
 import com.kamiltest.demo.manager.CarManager;
+import com.kamiltest.demo.manager.ClientManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/car/")
 public class CarController {
     private CarManager carManager;
+    private ClientManager clientManager;
 
     @Autowired
-    public CarController(CarManager carManager) {
+    public CarController(CarManager carManager, ClientManager clientManager) {
         this.carManager = carManager;
+        this.clientManager = clientManager;
     }
-    //update delete car
+
     @GetMapping("/getallcars")
     public String getAllCars(Model model){
         Iterable<Car> cars = this.carManager.findAll();
@@ -32,11 +34,30 @@ public class CarController {
         model.addAttribute("car",new Car());
         return "addCar";
     }
+    //that should be also update?
     @PostMapping("/addcar")
     public String addCarPost(@ModelAttribute("car")Car car){
-        //car without client
         this.carManager.updateCar(car);
-        return "redirect:/getallcars";
+        return "redirect:/api/car/getallcars";
+    }
+    //delete car
+    //if car has client it wont delete why?
+    @PostMapping("/deletecar/{id}")
+    public String deleteCar(@PathVariable Long id)
+    {
+        Optional<Car> carToDelete = this.carManager.findCarById(id);
+        if(carToDelete.isPresent())
+        {
+            if(carToDelete.get().getClient() != null)
+            {
+                this.clientManager.deleteCarForClientByCarId(id);
+                this.carManager.deleteCarById(id);
+            }
+            else{
+            this.carManager.deleteCarById(id);
+            }
+        }
+        return "redirect:/api/car/getallcars";
     }
 
 }
