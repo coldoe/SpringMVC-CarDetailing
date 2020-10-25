a075577d2bb5644e2a42b1c53eba46e806c09bf6
Iceberg <hawxhawx1920@gmail.com>
2020-10-25 20:34:07 +0100

    update,delete,get works with views for clients

--- src/main/java/com/kamiltest/demo/doa/controller/ClientController.java
+++ src/main/java/com/kamiltest/demo/doa/controller/ClientController.java
@@ -5,9 +5,7 @@ import com.kamiltest.demo.manager.ClientManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -26,9 +24,31 @@ public class ClientController {
         model.addAttribute("clients",this.clientManager.findAll());
         return "getAllClients";
     }
-
+    //maybe add something like show details
     @GetMapping("/getbyid")
     public Optional<Client> GetById(@RequestParam Long index){
         return this.clientManager.findById(index);
+    }
+
+    @GetMapping("/updateclient/{id}")
+    public String updateClient(@PathVariable Long id, Model model){
+        Optional<Client> clientToUpdate = this.clientManager.findById(id);
+        if(clientToUpdate.isPresent()){
+            model.addAttribute("clientToUpdate",clientToUpdate.get());
+            return "updateClient";
+        }
+        return "redirect:/api/client/getallclients";
+    }
+
+    @PutMapping("/updateclient/{id}")
+    public String updateClientToDatabase(@ModelAttribute("clientToUpdate")Client client){
+        this.clientManager.saveClient(client);
+        return "redirect:/api/client/getallclients";
+    }
+
+    @DeleteMapping("/deleteclient/{id}")
+    public String cascadeDeleteClient(@PathVariable Long id){
+        this.clientManager.delete(id);
+        return "redirect:/api/client/getallclients";
     }
 }
