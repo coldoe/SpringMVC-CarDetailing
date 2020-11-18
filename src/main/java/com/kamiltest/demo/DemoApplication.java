b90ab7a33b3b4b77d96ccc42ec9261d1c2d48607
Iceberg <hawxhawx1920@gmail.com>
2020-11-18 17:34:38 +0100

    fake inserting to database, without role

--- src/main/java/com/kamiltest/demo/DemoApplication.java
+++ src/main/java/com/kamiltest/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.kamiltest.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
-//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//		String raw = "admin";
-//		String pass = encoder.encode(raw);
-//		System.out.println("Pass: "+pass);
-//		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- src/main/java/com/kamiltest/demo/manager/MyUserDetailsService.java
+++ src/main/java/com/kamiltest/demo/manager/MyUserDetailsService.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
 public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -30,4 +34,15 @@ public class MyUserDetailsService implements UserDetailsService {
         }
         return null;
     }
+
+//    quick assign user to database but without role
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void addStartUsersToDb()
+//    {
+//        User admin = new User();
+//        admin.setEnabled(true);
+//        admin.setUsername("work");
+//        admin.setPassword(passwordEncoder.encode("work"));
+//        this.userRepo.save(admin);
+//    }
 }
